5509af6176ec7b8ccbdedd7f592aadd128b471e5
Filip Woźniak <filip.wozniak98@wp.pl>
2023-03-05 21:23:21 +0100

    added Vite to project

---
+++ src/ts/main.ts
@@ -0,0 +1,7 @@
+
+import '../sass/main.scss'
+// const customerInput: HTMLInputElement = document.querySelector('#user');
+// const companyInput: HTMLInputElement = document.querySelector('#company');
+
+
+console.log('halo1234567');
