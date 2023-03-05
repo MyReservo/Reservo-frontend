c37fae18a3a75e74c56fbcff4eecae68c1f007e0
Filip Woźniak <filip.wozniak98@wp.pl>
2023-03-05 23:07:17 +0100

    downloaded inputs in ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,40 @@
 
 import '../sass/main.scss'
-// const customerInput: HTMLInputElement = document.querySelector('#user');
-// const companyInput: HTMLInputElement = document.querySelector('#company');
+
+const customerInput: HTMLInputElement | null = document.querySelector('#user');
+const companyInput: HTMLInputElement | null = document.querySelector('#company');
 
 
-console.log('halo12345678');
+const nameInput: HTMLInputElement = document.querySelector('#name')!;
+const surnameInput = document.querySelector('#surname')!;
+
+const numberInput = document.querySelector('#phone')!;
+
+const emailInput = document.querySelector('#email');
+
+const passwordInput = document.querySelector('#password');
+
+const repeatInput = document.querySelector('#repeat');
+
+
+
+const termsInput = document.querySelector("#terms");
+
+const registerBtn: HTMLButtonElement| null = document.querySelector(".register");
+
+const nameBox: HTMLElement | null = document.querySelector('.basic-info-data');
+
+
+const inputsValidation = (e) => void{
+    e.preventDefault();
+    if(nameInput.value === ''){
+        const nameError: HTMLParagraphElement = document.createElement('p');
+        nameError.innerHTML = 'Podaj imię';
+        nameBox.appendChild(nameError);
+    }
+
+
+}
+
+
+registerBtn.addEventListener('click',  inputsValidation)
