2a76344399342975400379443eb1cee3294669fe
Filip Woźniak <filip.wozniak98@wp.pl>
2023-03-07 14:55:23 +0100

    aded validation form without checkbox inputs

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,40 +1,71 @@
 
 import '../sass/main.scss'
 
-const customerInput: HTMLInputElement | null = document.querySelector('#user');
-const companyInput: HTMLInputElement | null = document.querySelector('#company');
+// const customerInput = document.querySelector('#user') as HTMLInputElement ;
+// const companyInput = document.querySelector('#company') as HTMLInputElement;
+
+const nameInput = document.querySelector('#name') as HTMLInputElement;
+const surnameInput = document.querySelector('#surname') as HTMLInputElement;
+
+const numberInput = document.querySelector('#phone') as HTMLInputElement;
+const emailInput = document.querySelector('#email') as HTMLInputElement;
+
+const passwordInput = document.querySelector('#password') as HTMLInputElement;
+const repeatInput = document.querySelector('#repeat') as HTMLInputElement;
+
+// const termsInput = document.querySelector("#terms") as HTMLInputElement;
+
+const registerBtn = document.querySelector(".register") as  HTMLButtonElement;
+
+// const nameBox = document.querySelector('.basic-info-data') as HTMLElement;
 
 
-const nameInput: HTMLInputElement = document.querySelector('#name')!;
-const surnameInput = document.querySelector('#surname')!;
 
-const numberInput = document.querySelector('#phone')!;
+const toggleError = (input: HTMLInputElement, isError: boolean, placeholderText?: string): void => {
+  if (isError) {
+    input.setAttribute('placeholder', placeholderText ?? "Musisz podać jakąś wartość");
+    input.classList.add('red-input');
+    input.classList.add('red-placeholder');
+  } else {
+    input.removeAttribute('placeholder');
+    input.classList.remove('red-input');
+    input.classList.remove('red-placeholder');
+  }
+};
 
-const emailInput = document.querySelector('#email');
-
-const passwordInput = document.querySelector('#password');
-
-const repeatInput = document.querySelector('#repeat');
-
-
-
-const termsInput = document.querySelector("#terms");
-
-const registerBtn: HTMLButtonElement| null = document.querySelector(".register");
-
-const nameBox: HTMLElement | null = document.querySelector('.basic-info-data');
-
-
-const inputsValidation = (e) => void{
-    e.preventDefault();
-    if(nameInput.value === ''){
-        const nameError: HTMLParagraphElement = document.createElement('p');
-        nameError.innerHTML = 'Podaj imię';
-        nameBox.appendChild(nameError);
+const inputsValidation = (e: Event): void => {
+  e.preventDefault();
+  const regExPhone: RegExp = /^(?:\(?\?)?(?:[-\.\(\)\s]*(\d)){9}\)?$/
+  const regExEmail: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+  if (nameInput.value === '') {
+    toggleError(nameInput, true, 'Musisz podać imię');
+  }
+  if (surnameInput.value === '') {
+    toggleError(surnameInput, true, 'Musisz podać nazwisko');
+  } 
+  if (!regExPhone.test(numberInput.value)) {
+    toggleError(numberInput, true, 'Musisz podać numer telefonu');
+    } 
+    if(!regExEmail.test(emailInput.value)){
+        toggleError(emailInput,true, 'Musisz podać adres e-mail');
     }
+    if(passwordInput.value.length < 8){
+        toggleError(passwordInput,true, 'Hasło musi zawierać min. 8 znaków');
+    }
+    if(repeatInput.value !== passwordInput.value){
+        toggleError(repeatInput,true, 'Hasła muszą być takie same');
+    }
+};
 
+const removeError = (input: HTMLInputElement): void => {
+    toggleError(input,false)
+};
 
-}
-
-
-registerBtn.addEventListener('click',  inputsValidation)
+nameInput.addEventListener('input', () => removeError(nameInput));
+surnameInput.addEventListener('input', () => removeError(surnameInput));
+numberInput.addEventListener('input', () => removeError(numberInput));
+emailInput.addEventListener('input', () => removeError(emailInput));
+passwordInput.addEventListener('input', () => removeError(passwordInput));
+repeatInput.addEventListener('input', () => removeError(repeatInput));
+registerBtn.addEventListener('click', inputsValidation);
+  
