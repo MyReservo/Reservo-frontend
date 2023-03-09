bb94fcd00901fc87af9c3b592052fa91c91bc9c1
Filip Woźniak <filip.wozniak98@wp.pl>
2023-03-10 00:11:30 +0100

    made code more clean  in ts and css

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,9 +1,7 @@
-
-import '../sass/main.scss'
-
+import '../sass/main.scss';
 
 // const customerInput = document.querySelector('#user') as HTMLInputElement ;
-const companyInput = document.querySelector('#company') as HTMLInputElement;
+const checkCompanyInput = document.querySelector('#company') as HTMLInputElement;
 
 const nameInput = document.querySelector('#name') as HTMLInputElement;
 const surnameInput = document.querySelector('#surname') as HTMLInputElement;
@@ -14,256 +12,212 @@ const emailInput = document.querySelector('#email') as HTMLInputElement;
 const passwordInput = document.querySelector('#password') as HTMLInputElement;
 const repeatInput = document.querySelector('#repeat') as HTMLInputElement;
 
-const termsInput = document.querySelector("#terms") as HTMLInputElement;
-const termsBox = document.querySelector(".register-terms") as HTMLElement;
+const termsInput = document.querySelector('#terms') as HTMLInputElement;
+const termsBox = document.querySelector('.register-terms') as HTMLElement;
 
-const registerBtn = document.querySelector(".register") as  HTMLButtonElement;
+const registerBtn = document.querySelector('.register') as HTMLButtonElement;
 
 const basicInfo = document.querySelector('.basic-info') as HTMLElement;
-
-
-
-const toggleError = (input: HTMLInputElement, isError: boolean, placeholderText?: string): void => {
-  if (isError) {
-    input.setAttribute('placeholder', placeholderText ?? "Musisz podać jakąś wartość");
-    input.classList.add('red-input');
-    input.classList.add('red-placeholder');
-  } else {
-    input.removeAttribute('placeholder');
-    input.classList.remove('red-input');
-    input.classList.remove('red-placeholder');
-  }
-};
-
-
-
 
 const termsError = document.createElement('p');
 termsError.classList.add('terms-error');
 
 const checkboxCheck = () => {
-    if(!termsInput.checked){  
-        termsError.innerHTML = "Musisz zaakceptować regulamin";
-        termsBox.appendChild(termsError);
-         } else if (termsInput.checked && termsBox.contains(termsError)) {
-            termsBox.removeChild(termsError);
-         }
-}
-
-const removeError = (input: HTMLInputElement) => {
-    toggleError(input,false)
+	if (!termsInput.checked) {
+		termsError.innerHTML = 'Musisz zaakceptować regulamin';
+		termsBox.appendChild(termsError);
+	} else if (termsInput.checked && termsBox.contains(termsError)) {
+		termsBox.removeChild(termsError);
+	}
 };
 
-
-
-
 const basicInfoCompany = document.createElement('div');
-basicInfoCompany.classList.add('basic-info-company')
+basicInfoCompany.className = 'basic-info-company';
 
 const basicInfoData = document.createElement('div');
-basicInfoData.classList.add('basic-info-data');
+basicInfoData.className = 'basic-info-data';
 
 const basicInfoData2 = document.createElement('div');
-basicInfoData2.classList.add('basic-info-data');
+basicInfoData2.className = 'basic-info-data';
 
-const label = document.createElement('label');
-label.setAttribute('for', "company-name");
-label.innerText = "Nazwa firmy";
+const companyNamelabel = document.createElement('label');
+companyNamelabel.htmlFor = 'company-name';
+companyNamelabel.innerText = 'Nazwa firmy';
 
-const inputCompany = document.createElement('input');
-inputCompany.classList.add('input');
-inputCompany.setAttribute('id','company-name');
-inputCompany.setAttribute('type','text');
+const companyInput = document.createElement('input');
+companyInput.className = 'input';
+companyInput.id = 'company-name';
+companyInput.type = 'text';
 
-const label2 = document.createElement('label');
-label2.setAttribute('for', "services");
-label2.innerText = "Świadczone usługi";
+const servicesLabel = document.createElement('label');
+servicesLabel.htmlFor = 'services';
+servicesLabel.innerText = 'Świadczone usługi';
+
+const services = [
+	{ value: 'default', label: 'Wybierz usługę' },
+	{ value: 'fryzjer', label: 'Fryzjer' },
+	{ value: 'kosmetyczka', label: 'Kosmetyczka' },
+	{ value: 'mechanik', label: 'Mechanik' },
+	{ value: 'hydraulik', label: 'Hydraulik' },
+];
 
 const select = document.createElement('select');
-select.setAttribute('id','services');
-select.classList.add('input');
+select.id = 'services';
+select.className = 'input';
 
-const option = document.createElement('option');
-option.setAttribute('value', 'default');
-option.selected = true;
-option.disabled = true;
-option.innerText = "Wybierz usługę";
+services.forEach(service => {
+	const option = document.createElement('option');
+	option.value = service.value;
+	option.innerText = service.label;
+	select.appendChild(option);
+});
 
-const option2 = document.createElement('option');
-option2.setAttribute('value', 'fryzjer');
-option2.innerText = "Fryzjer";
 
-const option5 = document.createElement('option');
-option5.setAttribute('value', 'kosmetyczka');
-option5.innerText = "Kosmetyczka";
 
-const option4 = document.createElement('option');
-option4.setAttribute('value', 'mechanik');
-option4.innerText = "Mechanik";
-
-const option3 = document.createElement('option');
-option3.setAttribute('value', 'hydraulik');
-option3.innerText = "Hydraulik";
+const adressFields = [
+	{ id: 'street', placeholder: 'Ulica' },
+	{ id: 'home', placeholder: 'Nr.domu' },
+	{ id: 'city', placeholder: 'Miasto' },
+	{ id: 'code', placeholder: 'Kod pocztowy' },
+];
 
 const companyAdress = document.createElement('div');
-companyAdress.classList.add('company-adress');
+companyAdress.className = 'company-adress';
 
 const companyAdressTitle = document.createElement('h2');
-companyAdressTitle.innerText = "Adres pocztowy firmy"
-companyAdressTitle.classList.add('company-adress-title')
+companyAdressTitle.className = 'company-adress-title';
+companyAdressTitle.innerText = 'Adres pocztowy firmy';
 
-const companyPlace = document.createElement('div');
-companyPlace.classList.add('company-adress-place');
+const companyAdressPlace = document.createElement('div');
+companyAdressPlace.className = 'company-adress-place';
 
-const companyAdressStreet = document.createElement('div');
-companyAdressStreet.classList.add('company-adress-data');
-const inputStreet = document.createElement('input');
-inputStreet.classList.add('input');
-inputStreet.setAttribute('type', 'text');
-inputStreet.setAttribute('id','street')
-inputStreet.setAttribute('placeholder', "Ulica")
+const toggleError = (input: HTMLInputElement, isError: boolean, placeholderText?: string): void => {
+	if (isError) {
+		input.setAttribute('placeholder', placeholderText ?? 'Musisz podać jakąś wartość');
+		input.classList.add('red-input');
+		input.classList.add('red-placeholder');
+	} else {
+		input.removeAttribute('placeholder');
+		input.classList.remove('red-input');
+		input.classList.remove('red-placeholder');
+	}
+};
+const removeError = (input: HTMLInputElement) => {
+	toggleError(input, false);
+};
 
-const companyAdressHome = document.createElement('div');
-companyAdressHome.classList.add('company-adress-data');
-const inputHome = document.createElement('input');
-inputHome.classList.add('input');
-inputHome.setAttribute('type', 'text');
-inputHome.setAttribute('id','home')
-inputHome.setAttribute('placeholder', "Nr. domu")
+const inputsEvents = (event: Event) => {
+	const input = event.target as HTMLInputElement;
+	if (input instanceof HTMLSelectElement) {
+		input.classList.remove('red-input');
+	} else {
+		removeError(input);
+		input.value = input.value.replace(/[^\d\W]/g, '');
+	}
+};
 
-const companyCityCode = document.createElement('div');
-companyCityCode.classList.add('company-adress-place');
+adressFields.forEach(field => {
+	const adressDiv = document.createElement('div');
+	adressDiv.className = 'company-adress-data';
+	const adressInput = document.createElement('input');
+	adressInput.className = 'input';
+	adressInput.type = 'text';
+	adressInput.id = field.id;
+	adressInput.placeholder = field.placeholder;
+	registerBtn.addEventListener('click', () => {
+		let placeholderText = '';
+		switch (adressInput.id) {
+			case 'street':
+				placeholderText = 'Podaj nazwę ulicy';
+				break;
+			case 'home':
+				placeholderText = 'Podaj numer domu';
+				break;
+			case 'city':
+				placeholderText = 'Podaj nazwę miasta';
+				break;
+			case 'code':
+				placeholderText = 'Podaj kod pocztowy';
+				break;
+			default:
+				break;
+		}
+		if (adressInput.value === '') {
+			toggleError(adressInput, true, placeholderText);
+		} else {
+			toggleError(adressInput, false);
+		}
+	});
+	adressDiv.appendChild(adressInput);
+	companyAdressPlace.appendChild(adressDiv);
+	adressInput.addEventListener('input', () => {
+		removeError(adressInput);
+	});
 
-const companyCity = document.createElement('div');
-companyCity.classList.add('company-adress-data');
-const inputCity = document.createElement('input');
-inputCity.classList.add('input');
-inputCity.setAttribute('type', 'text');
-inputCity.setAttribute('id','city')
-inputCity.setAttribute('placeholder', "Miasto")
+	if (adressInput.id === 'home' || adressInput.id === 'code') {
+		adressInput.addEventListener('input', inputsEvents);
+	}
+});
 
-
-const companyCode = document.createElement('div');
-companyCode.classList.add('company-adress-data');
-const inputCode = document.createElement('input');
-inputCode.classList.add('input');
-inputCode.setAttribute('type', 'text');
-inputCode.setAttribute('id','code')
-inputCode.setAttribute('placeholder', "Kod pocztowy")
-
+companyAdress.appendChild(companyAdressTitle);
+companyAdress.appendChild(companyAdressPlace);
 
 const addInputs = () => {
-  if (companyInput.checked) {
-    basicInfoData.appendChild(label);
-    basicInfoData.appendChild(inputCompany);
-    basicInfoData2.appendChild(label2);
-    select.appendChild(option);
-    select.appendChild(option2);
-    select.appendChild(option3);
-    select.appendChild(option4);
-    select.appendChild(option5);
-    basicInfoData2.appendChild(select);
-
-    companyAdressStreet.appendChild(inputStreet);
-    companyAdressHome.appendChild(inputHome);
-    companyPlace.appendChild(companyAdressStreet);
-    companyPlace.appendChild(companyAdressHome);
-
-    companyCity.appendChild(inputCity);
-    companyCode.appendChild(inputCode);
-    companyCityCode.appendChild(companyCity);
-    companyCityCode.appendChild(companyCode);
-
-    companyAdress.appendChild(companyAdressTitle);
-    companyAdress.appendChild(companyPlace);
-    companyAdress.appendChild(companyCityCode); 
-    
-        basicInfoCompany.appendChild(basicInfoData);
-        basicInfoCompany.appendChild(basicInfoData2);
-        basicInfo.appendChild(basicInfoCompany);
-
-    basicInfo.appendChild(companyAdress);
+  if (checkCompanyInput.checked) {
+    basicInfoData.append(companyNamelabel,companyInput);
+    basicInfoData2.append(servicesLabel,select);
+    basicInfoCompany.append(basicInfoData,basicInfoData2);
+    basicInfo.append(basicInfoCompany,companyAdress);
   } else {
     basicInfoCompany.remove();
   }
 }
 
+const inputsValidation = (e: Event) => {
+	e.preventDefault();
+	const regExPhone: RegExp = /^(?:\(?\?)?(?:[-\.\(\)\s]*(\d)){9}\)?$/;
+	const regExEmail: RegExp =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+	if (nameInput.value === '') {
+		toggleError(nameInput, true, 'Podaj imię');
+	}
+	if (surnameInput.value === '') {
+		toggleError(surnameInput, true, 'Podaj nazwisko');
+	}
+	if (!regExPhone.test(numberInput.value)) {
+		toggleError(numberInput, true, 'Podaj numer telefonu');
+	}
+	if (!regExEmail.test(emailInput.value)) {
+		toggleError(emailInput, true, 'Podaj adres e-mail');
+	}
+	if (passwordInput.value.length < 8) {
+		toggleError(passwordInput, true, 'Hasło musi zawierać min. 8 znaków');
+	}
+	if (repeatInput.value !== passwordInput.value) {
+		toggleError(repeatInput, true, 'Hasła muszą być takie same');
+	}
+	if (companyInput.value === '') {
+		toggleError(companyInput, true, 'Podaj nazwę firmy');
+	}
+	if (select.value === 'default') {
+		select.classList.add('red-input');
+	}
+	checkboxCheck();
+};
 
+checkCompanyInput.addEventListener('change', addInputs);
 
-const inputsValidation = (e: Event): void => {
-  e.preventDefault();
-  const regExPhone: RegExp = /^(?:\(?\?)?(?:[-\.\(\)\s]*(\d)){9}\)?$/
-  const regExEmail: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
-  if (nameInput.value === '') {
-    toggleError(nameInput, true, 'Podaj imię');
-  }
-  if (surnameInput.value === '') {
-    toggleError(surnameInput, true, 'Podaj nazwisko');
-  } 
-  if (!regExPhone.test(numberInput.value)) {
-    toggleError(numberInput, true, 'Podaj numer telefonu');
-    } 
-    if(!regExEmail.test(emailInput.value)){
-        toggleError(emailInput,true, 'Podaj adres e-mail');
-    }
-    if(passwordInput.value.length < 8){
-        toggleError(passwordInput,true, 'Hasło musi zawierać min. 8 znaków');
-    }
-    if(repeatInput.value !== passwordInput.value){
-        toggleError(repeatInput,true, 'Hasła muszą być takie same');
-    }
-    if (inputCompany.value === '') {
-      toggleError(inputCompany,true, 'Podaj nazwę firmy');
-    }
-    if (select.value === 'default') {
-      select.classList.add('red-input');
-    } 
-    if (inputStreet.value === '') {
-      toggleError(inputStreet,true, 'Podaj nazwę ulicy');
-    }
-    if (inputHome.value === '') {
-      toggleError(inputHome,true, 'Podaj numer domu');
-    }
-    if (inputCity.value === '') {
-      toggleError(inputCity,true, 'Podaj nazwę miasta');
-    }
-    if (inputCode.value === '') {
-      toggleError(inputCode,true, 'Podaj kod pocztowy');
-    }
-    checkboxCheck()
-  };
-  
-  
-  const inputsEvents = (event:Event) => {
-    const input = event.target as HTMLInputElement;
-      if (input instanceof HTMLSelectElement) {
-        input.classList.remove('red-input');
-        console.log('działam1');
-      } else {
-        removeError(input);
-        input.value = input.value.replace(/[^\d\W]/g,'');
-        console.log('działam2');
-      }
-  }
-  
-  companyInput.addEventListener('change',addInputs);
-  nameInput.addEventListener('input', () => removeError(nameInput));
-  surnameInput.addEventListener('input', () => removeError(surnameInput));
-  numberInput.addEventListener('input', () => removeError(numberInput));
-  numberInput.addEventListener('keypress', function(e){
-    if (isNaN(parseInt(e.key))) {
-        e.preventDefault();
-    }
+nameInput.addEventListener('input', () => removeError(nameInput));
+surnameInput.addEventListener('input', () => removeError(surnameInput));
+numberInput.addEventListener('input', () => removeError(numberInput));
+numberInput.addEventListener('keypress', function (e) {
+	if (isNaN(parseInt(e.key))) {
+		e.preventDefault();
+	}
 });
 emailInput.addEventListener('input', () => removeError(emailInput));
-inputCompany.addEventListener('input', () => removeError(inputCompany));
-inputCity.addEventListener('input', () => removeError(inputCity));
-inputStreet.addEventListener('input',() => removeError(inputStreet));
- 
-      inputHome.addEventListener('input', inputsEvents);
-      select.addEventListener('input', inputsEvents);
-      inputCode.addEventListener('input', inputsEvents);
-
+companyInput.addEventListener('input', () => removeError(companyInput));
+select.addEventListener('input', inputsEvents);
 passwordInput.addEventListener('input', () => removeError(passwordInput));
 repeatInput.addEventListener('input', () => removeError(repeatInput));
-registerBtn.addEventListener('click', inputsValidation)
+registerBtn.addEventListener('click', inputsValidation);
