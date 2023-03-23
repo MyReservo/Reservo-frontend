2eeecbe3d5b2076b56567c2e286d40d362b537a1
Filip Woźniak <filip.wozniak98@wp.pl>
2023-03-24 00:29:06 +0100

    fixed validation in calandar.ts

--- src/ts/calendar.ts
+++ src/ts/calendar.ts
@@ -1,130 +1,5 @@
 export{}
 
-// let date = new Date();
-// const monthArr = new Array('Styczeń', 'Luty','Marzec', 'Kwiecień','Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień');
-
-// const hoursContainer: HTMLElement | null = document.querySelector('.hours-container');
-// const allTd: NodeListOf<Element>= document.querySelectorAll('tbody td');
-
-// const calendar = () => {
-//     const handleClick = (event: MouseEvent) => {
-//         const td = event.target as HTMLElement;
-//         const pickedDay = Number(td.textContent);
-//         console.log('warunek');
-//         if (new Date(date.getFullYear(), date.getMonth(), pickedDay) < date) {
-//             alert('Ten dzień już minał!');
-//             return;
-//         }
-    
-//         console.log("ok, to jest przyszłość");
-//         hoursContainer!.style.display = 'block';
-//     }
-        
-//     allTd.forEach(td => {
-//         td.addEventListener('click', handleClick);
-//     });
-
-//     const exitIcon = document.querySelector('.fa-times');
-// exitIcon?.addEventListener('click', () => {
-//     hoursContainer!.style.display = "none";
-// })
-
-// allTd.forEach(td => {
-
-//     td.removeEventListener('click', handleClick);
-
-
-// })
-    
-//     const monthAndYear = document.querySelector('#calendar-top') as HTMLElement;
-//     monthAndYear.innerText = monthArr[date.getMonth()] + " " + date.getFullYear();
-//     // console.log(monthArr[date.getMonth()] + " " + date.getFullYear() + " " + date.getDate());
-    
-   
-//     const firstDay = new Date(date.getFullYear(),date.getMonth(),1 )
-//     const lastDay = new Date(date.getFullYear(),date.getMonth() + 1, 0 )
-
-//     let day = 1
-
-//    for (let i = 0; i < allTd.length; i++) {
-//     allTd[i].textContent = i.toString();
-
-//     if (i <firstDay.getDay() -1) {
-//         allTd[i].innerHTML = '';
-//     } else if (day <= lastDay.getDate()) {
-//         allTd[i].innerHTML= day.toString();
-//         day++
-//     } else {
-//         allTd[i].innerHTML = '';
-//     } 
-//    }
-   
-//    allTd.forEach(td => {
-//        td.addEventListener('click', ()=> {
-//         const pickedDay = Number(td.textContent);
-//         console.log('warunek');
-//         if (new Date(date.getFullYear(), date.getMonth(), pickedDay) < date) {
-//             alert('Ten dzień już minał!');
-//             return;
-//         } 
-         
-//             console.log("ok, to jest przyszłość");
-//             hoursContainer!.style.display = 'block';
-//             // const pickedDay = td.textContent;
-//             // console.log(pickedDay);   
-        
-//     })
-//    })
-// }
-
-
-
-
-
-
-// const prevMonth = () => {
-//     date = new Date(date.getFullYear(), date.getMonth()-1,1)
-//     calendar()
-// }
-// const nextMonth = () => {
-//     date = new Date(date.getFullYear(), date.getMonth()+1,1)
-//     calendar()
-// }
-
-// document.querySelector('#prev')?.addEventListener('click', prevMonth);
-// document.querySelector('#next')?.addEventListener('click', nextMonth);
-
-// const hourBtns: NodeListOf<Element> = document.querySelectorAll('.hour-btn');
-
-// hourBtns.forEach(btn => {
-//     btn.addEventListener('click', () => {
-//         if (btn.classList.contains('active')) {
-//             btn.classList.remove('active'); 
-//         } else {
-//             hourBtns.forEach(btn => {
-//                 btn.classList.remove('active');
-//             })
-//                 btn.classList.add('active'); 
-//         }
-//         // const pickedHour = btn.textContent
-//         // console.log(pickedHour);
-//     })
-// })
-
-// // const exitIcon = document.querySelector('.fa-times');
-// // exitIcon?.addEventListener('click', () => {
-// //     hoursContainer!.style.display = "none";
-// // })
-
-
-
-// calendar()
-
-
-
-
-
-
 
 
 let date = new Date();
@@ -148,26 +23,26 @@ const handleClick = (event:Event) => {
     
     const today = new Date();
     const pickedDate = new Date(date.getFullYear(), date.getMonth(), pickedDay);
+    console.log(today.getHours(), today.getMinutes());
+    console.log(pickedDate.getDate());
+    console.log(today.getDate());
 
-    if (pickedDate < today && td.textContent !== "") {
+    if (td.textContent !== '' && pickedDate.getDate() == today.getDate() ) {
+        hoursContainer!.style.display = 'block';
+    } else if  (pickedDate < today && td.textContent !== "") {
         alert('Ten dzień już minął!');
         return;
-    } else if (td.textContent !== '') {
-        hoursContainer!.style.display = 'block';
-    }
+    } 
 };
 
 
 const calendar = () => {
     
     
-    
     const exitIcon = document.querySelector('.fa-times');
     exitIcon?.addEventListener('click', () => {
         hoursContainer!.style.display = "none";
-    });
-    console.log('jeden');
-    
+    });    
     const monthAndYear = document.querySelector('#calendar-top') as HTMLElement;
     console.log(monthAndYear.textContent);
     monthAndYear.textContent = monthArr[date.getMonth()] + " " + date.getFullYear();
@@ -194,8 +69,6 @@ const calendar = () => {
     
 
 };
-
-
 
 
 const prevMonth = () => {
@@ -227,8 +100,6 @@ hourBtns.forEach(btn => {
             })
                 btn.classList.add('active'); 
         }
-        // const pickedHour = btn.textContent
-        // console.log(pickedHour);
     })
 })
 
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+// import vitePluginSass from 'vite-plugin-sass';
 
 export default defineConfig({
-	base: '/Reservo-frontend/',
-	plugins: [tsconfigPaths()],
-	optimizeDeps: {
-	  include: ['main.ts', 'new-elements.ts']
-	}
+  base: '/Reservo-frontend/',
+  plugins: [
+    tsconfigPaths(),
+    // vitePluginSass({
+    //   include: /src\/.*\.scss$/,
+    //   exclude: /node_modules/,
+    // }),
+  ],
+  optimizeDeps: {
+    include: ['main.ts', 'new-elements.ts', 'calendar.ts']
+  },
+//   build: {
+//     css: {
+//       extract: {
+//         filename: 'calendar.css'
+//       }
+//     }
+//   },
+//   resolve: {
+//     alias: {
+//       '@calendar': '\OneDrive\Pulpit\Programming\Reservo-front\app\src\sass\calendar.scss'
+//     }
+//   }
 });
