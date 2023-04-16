e034b3e8d1da677252a291f0d2566e2b2d90a964
Filip Woźniak <filip.wozniak98@wp.pl>
2023-04-16 13:55:00 +0200

    fixed z index of burger btn and nav

--- src/ts/nav-elements.ts
+++ src/ts/nav-elements.ts
@@ -1,13 +1,25 @@
-export{};
+export {};
 
-const burgerBtn = document.querySelector("#burger-btn") as HTMLButtonElement;
+const burgerBtn = document.querySelector('#burger-btn') as HTMLButtonElement;
+const burgerBtnMobile = document.querySelector('.mobile-btn') as HTMLButtonElement;
+const nav = document.querySelector('#mobile-nav');
+const body = document.querySelector('body');
 
-if(burgerBtn){
+if (burgerBtn) {
+	const burgerBtns = [burgerBtn, burgerBtnMobile];
 
-    burgerBtn!.addEventListener('click', () => {
-    
-        console.log('kliknieto');
-        burgerBtn!.classList.toggle('open');
-    
-    })
+	burgerBtns.forEach(btn => {
+		btn!.addEventListener('click', () => {
+			btn!.classList.toggle('open');
+			console.log('nadaje nav--active na navigacje');
+			nav!.classList.toggle("nav--active");
+			body!.classList.toggle('body-overflow');
+
+		});
+	});
 }
+
+
+
+
+
