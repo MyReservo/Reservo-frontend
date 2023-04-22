9670150be426e0e45de89971d9f4ce3837bbdd09
Filip Woźniak <filip.wozniak98@wp.pl>
2023-04-22 23:38:05 +0200

    changed url of background image

--- src/ts/company-panel.ts
+++ src/ts/company-panel.ts
@@ -1,7 +1,5 @@
-
-export{}
+export {};
 // import {prevMonth,nextMonth} from "./customer-panel";
-
 
 const yourProfessionTitle = document.querySelector('#profession-name') as HTMLSpanElement;
 const userNameNav = document.querySelector('.user-name') as HTMLParagraphElement;
@@ -14,7 +12,6 @@ const code = document.querySelector('#visit__adress-code') as HTMLParagraphEleme
 
 const heroCompanyImg = document.querySelector('.hero-img') as HTMLParagraphElement;
 
-
 if (yourProfessionTitle) {
 	yourProfessionTitle.textContent = localStorage.getItem('selectedOptionText');
 }
@@ -23,65 +20,73 @@ if (userNameNav) {
 	userNameNav.textContent = localStorage.getItem('name');
 }
 
-   localStorage.getItem('serviceDay')
-   localStorage.getItem("selectedOption");
-   console.log(localStorage.getItem("selectedOption"));
-   localStorage.getItem('companyStreet');
-   localStorage.getItem("companyHome");
-   localStorage.getItem("companyCity");
-   localStorage.getItem("companyCode");
+localStorage.getItem('serviceDay');
+localStorage.getItem('selectedOption');
+console.log(localStorage.getItem('selectedOption'));
+localStorage.getItem('companyStreet');
+localStorage.getItem('companyHome');
+localStorage.getItem('companyCity');
+localStorage.getItem('companyCode');
 
 if (userName) {
-	userName.textContent =  "Imię klienta: " + localStorage.getItem('name');
+	userName.textContent = 'Imię klienta: ' + localStorage.getItem('name');
 }
-if(street){
-	street.textContent =   localStorage.getItem('companyStreet');
+if (street) {
+	street.textContent = localStorage.getItem('companyStreet');
 }
-if(house){
-	house.textContent =   localStorage.getItem('companyHome');
+if (house) {
+	house.textContent = localStorage.getItem('companyHome');
 }
-if(city){
-	city.textContent =   localStorage.getItem('companyCity');
+if (city) {
+	city.textContent = localStorage.getItem('companyCity');
 }
-if(code){
-	code.textContent =   localStorage.getItem('companyCode');
+if (code) {
+	code.textContent = localStorage.getItem('companyCode');
 }
 
 const combIcon = document.querySelector('.comb') as HTMLElement;
 const clipperIcon = document.querySelector('.clipper') as HTMLElement;
 const scissorsIcon = document.querySelector('.scissors-company') as HTMLElement;
-const barberIcons = [combIcon,clipperIcon,scissorsIcon];
+const barberIcons = [combIcon, clipperIcon, scissorsIcon];
 
 const scredriverIcon = document.querySelector('.screwdriver') as HTMLElement;
 const gearIcon = document.querySelector('.gear') as HTMLElement;
 const wrenchIcon = document.querySelector('.wrench-company') as HTMLElement;
-const mechanicIcons = [scredriverIcon,gearIcon,wrenchIcon];
+const mechanicIcons = [scredriverIcon, gearIcon, wrenchIcon];
 
 const bookIcon = document.querySelector('.book') as HTMLElement;
 const graduationIcon = document.querySelector('.graduation') as HTMLElement;
 const pencilIcon = document.querySelector('.pencil-company') as HTMLElement;
-const tutorIcons = [bookIcon,graduationIcon,pencilIcon];
-   
+const tutorIcons = [bookIcon, graduationIcon, pencilIcon];
 
-if (localStorage.getItem("selectedOption") === "BARBER") {
-	heroCompanyImg.style.backgroundImage = "url('/Reservo-frontend/src/img/barber-large.jpg')";
-
+if (localStorage.getItem('selectedOption') === 'BARBER') {
+	if (heroCompanyImg) {
+		heroCompanyImg.style.backgroundImage = "url('./src/img/barber-large.jpg')";
+	}
 	barberIcons.forEach(icon => {
-		icon.style.display = "block";
-	})
+		if(icon){
+			icon.style.display = 'block';
+		}
+	});
 }
 
-if (localStorage.getItem("selectedOption") === "TUTOR") {
-	heroCompanyImg.style.backgroundImage = "url('/Reservo-frontend/src/img/tutor-big.jpg')";
+if (localStorage.getItem('selectedOption') === 'TUTOR') {
+	if (heroCompanyImg) {
+		heroCompanyImg.style.backgroundImage = "url('../src/img/tutor-big.jpg')";
+	}
 	tutorIcons.forEach(icon => {
-		icon.style.display = "block";
-	})
+		if(icon){
+			icon.style.display = 'block';
+		}
+	});
 }
-if (localStorage.getItem("selectedOption") === "MECHANIC") {
-	heroCompanyImg.style.backgroundImage = "url('/Reservo-frontend/src/img/mechanic-big.jpg')";
+if (localStorage.getItem('selectedOption') === 'MECHANIC') {
+	if (heroCompanyImg) {
+		heroCompanyImg.style.backgroundImage = "url('/src/img/mechanic-big.jpg')";
+	}
 	mechanicIcons.forEach(icon => {
-		icon.style.display = "block";
-	})
+		if(icon){
+			icon.style.display = 'block';
+		}
+	});
 }
-
-
