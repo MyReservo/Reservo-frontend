4e89effbd615034a665052177d369d2068e4f05b
Filip Woźniak <filip.wozniak98@wp.pl>
2023-09-07 12:33:22 +0200

    added live icon

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 
-describe('Rejestracja', () => {
-  it('Prawidłowa rejestracja klienta', () => {
+describe('Registration', () => {
+  it('Correct customer registration', () => {
     cy.visit('/');
     cy.get('#name').type("Name").should('have.value', 'Name');
     cy.get('#surname').type("surname").should('have.value', 'surname');
@@ -14,11 +14,10 @@ describe('Rejestracja', () => {
     it('Selecting the time of the visit', () => {
       // cy.visit('calendar.html')
       cy.get('.user-name').should('contain', "Name");
-
     })
   })
 
-  it("Nieprawidłowa rejestracja klienta", () => {
+  it("Incorrect customer registration", () => {
     cy.visit('/');
     cy.get('#register').click();
     cy.get('#name').should('have.class', 'red-input');
@@ -29,26 +28,28 @@ describe('Rejestracja', () => {
     cy.get('.register-terms').find('.terms-error').should('exist');
   })
 
-  it("Prawidłowa rejestracja firmy", () => {
+  it("Correct company registration", () => {
     cy.visit('/');
     cy.get('#company').check();
     cy.get('#provider-name').type("Name").should('have.value', 'Name');
     cy.get('#surname').type("surname").should('have.value', 'surname');
     cy.get('#phone').type('123456789').should('have.value', '123456789');
     cy.get('#email-company').type("test@wp.pl").should('have.value', 'test@wp.pl');
-    cy.get('#password').type("12345678").should('have.value', '12345678');
-    cy.get('#repeat').type("12345678").should('have.value', '12345678');
-    cy.get('#terms').check().should('be.checked');
-
+    
     cy.get('#company-name').type("Company").should("have.value", "Company");
+    cy.get('#services').should('not.be.empty');
+
     cy.get('#street').type("Street").should("have.value", "Street");
     cy.get('#home').type("123").should("have.value", "123");
     cy.get('#citySelect').should('not.be.empty');
-    cy.get('#services').should('not.be.empty');
     cy.get('#code').type("12-345").should('have.value', "12-345");
+
+    cy.get('#password').type("12345678").should('have.value', '12345678');
+    cy.get('#repeat').type("12345678").should('have.value', '12345678');
+    cy.get('#terms').check().should('be.checked');
   })
 
-  it("Nieprawidłowa rejestracja firmy", () => {
+  it("Incorrect company registration", () => {
     cy.visit('/');
     cy.get('#company').check();
     cy.get('#register').click();
@@ -57,9 +58,6 @@ describe('Rejestracja', () => {
     cy.get('#surname').should('have.class', 'red-input');
     cy.get('#phone').should('have.class', 'red-input');
     cy.get('#email-company').should('have.class', 'red-input');
-    cy.get('#password').should('have.class', 'red-input');
-
-    cy.get('#terms').check().should('be.checked');
 
     cy.get('#company-name').should("have.class", "red-input");
     cy.get('#street').should("have.class", "red-input");
@@ -67,5 +65,22 @@ describe('Rejestracja', () => {
     cy.get('#citySelect').should('not.be.empty');
     cy.get('#services').should('not.be.empty');
     cy.get('#code').should('have.class', "red-input");
+    cy.get('#password').should('have.class', 'red-input');
+    cy.get('#terms').check().should('be.checked');
+  })
+})
+
+describe("Login", () => {
+
+  it("Correct login", () => {
+    cy.visit('/');
+    cy.get(".login__text--link").click();
+    cy.get('#email-login').type("test@gmail.com").should("have.value", "test@gmail.com");
+    cy.get('#password-login').type('12345678').should("have.value", "12345678");
+  })
+  it("Incorrect login", () => {
+    cy.get('#login-btn').click();
+    cy.get('#email-login').should("have.class", "red-input");
+    cy.get('#password-login').should("have.class", "red-input" );
   })
 })
